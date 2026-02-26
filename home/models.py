0b40a68c07aa6eddc70f0db577947fd2220a6782
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-02-27 03:52:18 +0530

    66799

--- home/models.py
+++ home/models.py
@@ -16,3 +16,12 @@ class Restaurent(models.Model):
     has_delivery = models.BooleanField(default=False)
     def__str__(self):
         return self.name        
+
+class MenuItem(models.Model):
+    name = modelss.CharField(max_length=100)
+    price = models.DecimalField(max_digit=8,decimal_places=2)
+    category = models.ForeignKey(MenuCategory,on_delete=models.CASCADE)
+    is_featured = models.BooleanField(default=False)
+
+    def__str__(self):
+        return self.name
