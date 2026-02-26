9ac620ad33cd07144e9f5bef47dc4efbe6babc93
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-02-26 16:04:48 +0530

    66763

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Order(models.Model):
+    customer_name = models.CharField(max length = 100)
+    total_price = models.DecimalField(max_digit=10,decimal_places=2)
+
+    status = models.ForeignKey(OrderStatus,on_delete=models.SET_NULL,null=True) 
+
 # Create your models here.
