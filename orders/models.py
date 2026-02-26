9066101fd55ea7374f4b5f2a3453813a16484a35
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-02-27 03:32:43 +0530

    66798

--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class OrderStatus(models.Model):
+    name = models.CharField(max_length=50,unique=True)
+
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
-    customer_name = models.CharField(max length = 100)
+    user = ForeignKey(User,on_delete=models.CASCADE)
+    
     total_price = models.DecimalField(max_digit=10,decimal_places=2)
 
     status = models.ForeignKey(OrderStatus,on_delete=models.SET_NULL,null=True) 
@@ -9,14 +17,10 @@ class Order(models.Model):
 # Create your models here.
 
 
-class OrderStatus(model.Model):
-    name = models.CharField(max_length=50,unique=True)
+def __str__(self):
+    return f"Oreder{self,id}"
 
-    def __str__(self):
-        return self.name
-
-
-class Coupon(models.model):
+class Coupon(models.Model):
     code = models.CharField(max_length=50,unique=True)
     discount_percentage = models.DecimalField(max_digit=5,decimal_places=2)
     is_active = models.BooleanField(default=True)
