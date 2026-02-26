a00e2b0fd6293c150aa0b6061e088ab62fe38e1f
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-02-27 03:18:55 +0530

    66797

---
+++ home/utils.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from .models import DailyOperatingHours
+
+def get_today_operating_hours():
+    today = datetime.now().strftime("%A")
+
+    try:
+        hours = DailyOperatingHours.object.get(day=today)
+        return (hours.open_time,hours.close_time)
+
+    except DailyOperatingHours.DoesNotExist:
+        return (None,None)
