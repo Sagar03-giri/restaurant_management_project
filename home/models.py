f25b3e617bb94dae26999e60e2514ce4462b3419
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-02-27 03:06:14 +0530

    66796

--- home/models.py
+++ home/models.py
@@ -8,3 +8,11 @@ class MenuCategory(models.Model):
 
     def __str__(self):
         return self.name    #i have done this in vs.code
+
+
+class Restaurent(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    has_delivery = models.BooleanField(default=False)
+    def__str__(self):
+        return self.name        
