6e4ca277102a386872df5773ff47a0744edd3551
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-02-27 13:15:35 +0530

    66837

--- home/views.py
+++ home/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
-from .models import MenuCategory
-from .serializers import MenuCategorySerializer
+from rest_framework import viewset , status
+from rest_framework import Response
+from .models import MenuCategory , MenuItem
+from .serializers import MenuCategorySerializer , MenuItemSerializer
 
 # Create your views here.
 
 class MenuCategoryListView(ListAPIView):
     queryset = MenuCategory.objects.all()
     serializer_class = MenuCategorySerializer
+
+
+class MenuItemUpdateViewSet(viewset.ViewSet):
+    permission_classes = [IsAdminUser]
+
+    def update(self , request ,pk=None):
+        try:
+            item = MenuItem.objects.get(pk=pk)
+        except MenuItem.DoesNotExist:
+            return Response(
+                {"error":"Menu item not found"},
+                status = status.HTTP_404_NOT_FOUND
+                )
+        serializer = MenuItemSerializer(item,data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors,status=status.HTTP_404_BAD_REQUEST)
