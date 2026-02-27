28a96f8c6e62e4a9de915c4b7bb9efa216b8f1fe
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-02-27 17:54:39 +0530

    66841

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from  .models import Order, OrderStatus
+
+def mark_orders_processed(modeladmin, request , queryset):
+    try:
+        processed_status = OrderStatus.objects.get(name="processed")
+        query.update(status=processed_status)
+
+    except OrderStatus.DoesNotExist:
+        modeladmin.message_user(
+            request,
+            "processed status does not exist ."
+        )
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id' , 'customer_name', 'status')
+    actions = [mark_orders_processed]
+
+admin.site.register(Order , OrderAdmin)
