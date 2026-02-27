329594e897406788b7b88809dd778e543c43fc9e
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-02-27 13:27:35 +0530

    66833

--- home/utils.py
+++ home/utils.py
@@ -5,8 +5,23 @@ def get_today_operating_hours():
     today = datetime.now().strftime("%A")
 
     try:
-        hours = DailyOperatingHours.object.get(day=today)
+        hours = DailyOperatingHours.objects.get(day=today)
         return (hours.open_time,hours.close_time)
 
     except DailyOperatingHours.DoesNotExist:
         return (None,None)
+
+def is_restaurant_open():
+    now = datetime.now()
+    current_timee = now.time()
+    current_day = now.weekday()
+
+    if current_day <= 4:
+        open_time = time(9,0)
+        close_time = time(22,0)
+
+    else:
+        open_time = time(10,0)
+        close_time = time(23,0)
+
+    return open_time <= current_time <= close_time
