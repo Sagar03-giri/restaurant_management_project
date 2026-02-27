2dfebdff47e27338cc4f787ab331851ec3202bec
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-02-27 13:51:05 +0530

    66834

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('menu-categories/',MenuCategoryListView.as_view(),name='menu-categories'),
+    path(
+        'api/menu-items/<int:pk>/ingredients/',
+        MenuItemIngredientsView.as_view(),
+        name = 'menuitem-ingredients'
+
+    ),
     
 ]
