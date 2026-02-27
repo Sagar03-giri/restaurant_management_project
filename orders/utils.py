d1cc2076fd9c1a1a9a0946bcbc3a168cc7984f45
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-02-27 11:45:07 +0530

    66813

--- orders/utils.py
+++ orders/utils.py
@@ -1,5 +1,6 @@
 import string
 import secrets 
+from decimal import Decimal, ROUND_HALF_UP
 from django.db.model import Sum
 from .models import Coupon, Order
 
@@ -17,3 +18,9 @@ def get_daily_sales_total(date):
     orders = Order.objects.filter(created_at__date=date)
     result = orders.aggregate(total_sum=Sum('total_price'))
     return result['total_sum'] or 0
+
+def calculate_tip_amount(order_total, tip_percentage):
+    order_total = Decimal(order_total)
+    tip = order_total * (Decimal(tip_percentage) / Decimal(100))
+
+return tip.quantize(Decimal("0.01"), rounding = ROUND_HALF_UP)
