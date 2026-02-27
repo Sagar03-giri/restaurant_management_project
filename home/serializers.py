d263682400726d9452ebd267fd6b2efa01a41e5b
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-02-27 13:01:22 +0530

    66837

--- home/serializers.py
+++ home/serializers.py
@@ -10,3 +10,8 @@ class  MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
         fields = '__all__'
+
+    def validate_price(self,value):
+        if value <= 0 :
+            raise serializers.ValidationError("price must be positive.")
+        resturn value
