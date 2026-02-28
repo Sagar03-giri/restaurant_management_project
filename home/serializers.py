f6328fd6b0f6e385817984045cbd81d4f884c507
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-02-28 13:51:25 +0530

    66845

--- home/serializers.py
+++ home/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import MenuCategory, MenuItem,ingredient
 from orders.models import Order
+from .models import Table
 
 class MenuCategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -27,3 +28,8 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
+
+class TableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Table
+        fields = ['table_number','capacity','is_available']
