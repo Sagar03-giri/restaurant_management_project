003eba93c8803d147d7552f3b08fa0875507110e
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-02-28 14:06:06 +0530

    66846

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import MenuCategory , MenuItem,Resturant
+
+class ResturantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address','phone_number','email')
+    search_fields = ('name','address')
+    list_filter = ('is_active',)
+
+admin.site.register(MenuCategory,   MenuCategoryAdmin)
+admin.site.site.register(Resturant,ResturantAdmin)
 
 # Register your models here.
