b606e0c98d88a1a0f69df3a1df1775b4aad7ad7f
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-02-28 13:56:15 +0530

    66845

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('menu-categories/',MenuCategoryListView.as_view(),name='menu-categories'),
+    path(
+        'menu-categories/',MenuCategoryListView.as_view(),
+        name='menu-categories'),
     path(
         'api/menu-items/<int:pk>/ingredients/',
         MenuItemIngredientsView.as_view(),
@@ -19,5 +21,12 @@ urlpatterns = [
         'api/atbles/<int:pk/' ,
         TableDetailView.as_view()
         name = 'table-detail'
+    ),
+
+    path (
+        'api/tables/available/',
+        AvailableTablesAPIView.as_view(),
+        name = 'available_tables_api'
+
     ),
 ]
