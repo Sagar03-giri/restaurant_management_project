64f9bd30dfae635032c05beb4180d3ad0b60f16c
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-02-28 14:22:22 +0530

    66847

--- orders/models.py
+++ orders/models.py
@@ -14,21 +14,27 @@ class OrderManager(models.Manager):
 class Order(models.Model):
     user = ForeignKey(User,on_delete=models.CASCADE)
     
-    total_price = models.DecimalField(max_digit=10,decimal_places=2)
+    total_price = models.DecimalField(max_digits=10,decimal_places=2)
 
     status = models.ForeignKey(OrderStatus,on_delete=models.SET_NULL,null=True) 
     
     objects = OrderManager()
 
+    def get_unique_item_names(self):
+        unique_names = set()
+        for order_item in self.orderitem_set.all():
+            unique_names.add(order_item.menu_item.name)
+        return list(unique_names)
+
     def __str__(self):
-    return f"Oreder{self,id}"
+    return f"Order{self,id}"
 
 class Coupon(models.Model):
     code = models.CharField(max_length=50,unique=True)
-    discount_percentage = models.DecimalField(max_digit=5,decimal_places=2)
+    discount_percentage = models.DecimalField(max_digits=5,decimal_places=2)
     is_active = models.BooleanField(default=True)
-    valid_from = models.DataField()
-    valid_until = models.DataField()
+    valid_from = models.DateField()
+    valid_until = models.DateField()
 
     def __str__(self):
         return self.code
