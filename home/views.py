2bc981331d448c8223878ede3e0ce358f05d3f5c
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-03-04 00:46:05 +0530

    66866

--- home/views.py
+++ home/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView,RetrieveAPIView
-from rest_framework import viewset , status
+from rest_framework import viewsets , status
 from rest_framework.response import Response
 from .models import MenuCategory , MenuItem
-from rest_framework.permission import IsAdminUser
-from .serializers import MenuCategorySerializer , MenuItemSerializer
-from .serializers import (MenuCategorySerializer,MenuItemSerializer,IngredientSerializer,)
-from rest_framework.views import ListAPIView
+from rest_framework.permissions import IsAdminUser
+#from .serializers import MenuCategorySerializer , MenuItemSerializer
+from .serializers import (MenuCategorySerializer,MenuItemSerializer,IngredientSerializer,TableSerializer)
+from rest_framework.generics import ListAPIView
 from .models import Table
-from .serializer import TableSerializer
+#from .serializers import TableSerializer
+from rest_framework.views import APIView
 # Create your views here.
 
 class MenuCategoryListView(ListAPIView):
@@ -16,7 +17,7 @@ class MenuCategoryListView(ListAPIView):
     serializer_class = MenuCategorySerializer
 
 
-class MenuItemUpdateViewSet(viewset.ViewSet):
+class MenuItemUpdateViewSet(viewsets.ViewSet):
     permission_classes = [IsAdminUser]
 
     def update(self , request ,pk=None):
@@ -56,7 +57,7 @@ class MenuItemsByCategoryView(APIView):
             items = MenuItem.objects.all()
 
         serializer = MenuItemSerializer(items, many = True)
-        rerurn Response (serializer.data)
+        return Response (serializer.data)
 
 class TableDetailView(RetrieveAPIView):
     queryset = Table.objects.all()
