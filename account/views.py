fe602b05e6d7741ccc8ea25b8e6fadbb7df59d70
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-03-04 14:44:05 +0530

    66865

--- account/views.py
+++ account/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.response import response
+from django.contrib.auth.models import User
+from home.serializers import UserProfileSerializer
 
+class UserProfileViewSet(viewset.ViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def update(self,request,pk=None):
+        user = request.user
+
+        if user.id != int(pk):
+            return Response(
+                {"error": "you can only update your own profile"},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        
+        serializer = UserProfileSerializer (user , data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors,status=status.HTTP_400_REQUEST)
 # Create your views here.
