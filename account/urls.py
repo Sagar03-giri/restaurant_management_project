256d42c3d4aa819f55020238ae1d26525ddaa706
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-03-04 14:46:36 +0530

    66865

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from .views import UserProfileViewSet
 from .views import *
 
 urlpatterns = [
+    path(
+        "profile/<int:pk>/",
+        UserProfileViewSet.as_view({"put":"update"}) , 
+        name = "update-profile"
+    ),
     
 ]
