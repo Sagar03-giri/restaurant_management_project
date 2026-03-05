4bc90ff88e02d6e51a992cb92307cf51a5c4a92f
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-03-05 12:39:52 +0530

    66950

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from decimal import Decimal
+from home.models import MenuItem
 
 class OrderStatus(models.Model):
     name = models.CharField(max_length=50,unique=True)
@@ -14,11 +16,17 @@ class OrderManager(models.Manager):
 class Order(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     
-    total_price = models.DecimalField(max_digits=10,decimal_places=2)
+    total_price = models.DecimalField(max_digits=10,decimal_places=2,default=0)
 
     status = models.ForeignKey(OrderStatus,on_delete=models.SET_NULL,null=True) 
     
     objects = OrderManager()
+
+    def calculate_total(self):
+        total = Decimal("0.00")
+        for item in self.orderitem_set.all():
+            total += item.price*item.quantity
+        return total
 
     def get_unique_item_names(self):
         unique_names = set()
@@ -28,6 +36,15 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order{self.id}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(order,on_delete=models.CASCADE)
+    menu_item = models.ForeignKey(MenuItem,on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+
+    def __str__(self):
+        return f"{self.menu_item.name} x {self.quantity}"
 
 class Coupon(models.Model):
     code = models.CharField(max_length=50,unique=True)
