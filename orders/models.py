80860a77946179dbedfb7dcafceb9fab6f90d1cf
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-03-05 15:24:38 +0530

    66948

--- orders/models.py
+++ orders/models.py
@@ -65,3 +65,11 @@ class LoyaltyProgram(models.Model):
 
     def __str__(self):
         return self.name
+
+class PaymentMethod(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
