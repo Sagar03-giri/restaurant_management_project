e54d000a71ac34fb7269b10d399854e119f6767c
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-03-06 13:19:12 +0530

    66963

--- orders/urls.py
+++ orders/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from .views import *
 from .views import CouponValidationView
 from .views import OrderDetailView
+from .views import PaymentMethodListAPIView
 
-urlpatterns = [path('coupons/validate/',CouponValidationView.as_view(),name='validate-coupon'),
+urlpatterns = [
+    path(
+        'coupons/validate/',CouponValidationView.as_view(),name='validate-coupon'
+        ),
     
 
 path(
@@ -11,4 +15,10 @@ path(
     OrderDetailView.as_view(),
     name='order-detail'
 ),    
+
+path(
+    'payment-method/' ,
+    PaymentMethodListAPIView.as_view(),
+    name = 'payment-methods'
+),
 ]
