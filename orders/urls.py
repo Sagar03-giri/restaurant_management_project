d9b5dfd08eb585d642574f74e312a267387d664e
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-03-06 15:28:56 +0530

    66964

--- orders/urls.py
+++ orders/urls.py
@@ -3,22 +3,28 @@ from .views import *
 from .views import CouponValidationView
 from .views import OrderDetailView
 from .views import PaymentMethodListAPIView
-
+from .views import CancelOrderAPIView
 urlpatterns = [
     path(
         'coupons/validate/',CouponValidationView.as_view(),name='validate-coupon'
         ),
     
 
-path(
-    'orders/<int:pk>',
-    OrderDetailView.as_view(),
-    name='order-detail'
-),    
+    path(
+        'orders/<int:pk>',
+        OrderDetailView.as_view(),
+        name='order-detail'
+    ),    
 
-path(
-    'payment-method/' ,
-    PaymentMethodListAPIView.as_view(),
-    name = 'payment-methods'
-),
+    path(
+        'payment-method/' ,
+        PaymentMethodListAPIView.as_view(),
+        name = 'payment-methods'
+    ),
+
+    path(
+        "cancel-order/<int:order_id>/",
+        CancelOrderAPIView.as_view(),
+        name="cancel-order"
+    ),
 ]
