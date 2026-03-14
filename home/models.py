66517ee3c0011b58d51567ae69d0b476090d7528
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-03-15 03:07:49 +0530

    66985

--- home/models.py
+++ home/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import User
 
 class MenuCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name    #i have done this in vs.code
