a5517b5f7f13ea28820d97418e5a7528b7ac11bc
Sagar03-giri <149563947+Sagar03-giri@users.noreply.github.com>
2026-03-24 22:47:45 +0530

    67461

--- orders/models.py
+++ orders/models.py
@@ -65,14 +65,7 @@ class Coupon(models.Model):
         return self.code
 
 
-class LoyaltyProgram(models.Model):
-    name = models.CharField(max_length=50,unique=True)
-    points_required = models.IntegerField(unique=True)
-    discount_percentage = models.DecimalField(max_digits=5,decimal_places=2)
-    description = models.TextField()
 
-    def __str__(self):
-        return self.name
 
 class PaymentMethod(models.Model):
     name = models.CharField(max_length=50, unique=True)
